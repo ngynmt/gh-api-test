8e030004a47f760e51db289a72cbd4008d1688ee
Michelle Nguyen <ngynmt@gmail.com>
2018-12-06 17:37:07 -0800

    implement code block editor

--- src/reducers/pagesReducer.js
+++ src/reducers/pagesReducer.js
@@ -1,9 +1,28 @@
-import { UPDATE_NAV, UPDATE_PAGES } from '../constants/actionConstants';
+import {
+  UPDATE_COMPONENT_ORDER,
+  UPDATE_PAGE_COMPONENT,
+  UPDATE_PAGE_SELECTED,
+  SAVE_CHANGES,
+  UPDATE_NAV,
+  UPDATE_PAGES,
+  UPDATE_CODEBLOCK_TAB
+} from '../constants/actionConstants';
 import navigation from '../data/mainList';
 
 const initialState = {
   navigation,
   selectedPage: navigation[1].pages[0], // select "API Initialization" for now
+  selectedHeader: navigation[1].header,
+  selectedComponent: {
+    type: 'CODEBLOCK',
+    index: 3,
+    content: [
+      {
+        language: 'javascript',
+        snippet: ''
+      }
+    ]
+  },
   pages: []
 };
 
@@ -19,6 +38,19 @@ export default (state = initialState, action) => {
       return {
         ...state,
         pages: []
+      };
+    case UPDATE_PAGE_COMPONENT:
+      return {
+        ...state,
+        selectedComponent: action.payload
+      };
+    case UPDATE_CODEBLOCK_TAB:
+      return {
+        ...state,
+        selectedComponent: {
+          ...state.selectedComponent,
+          content: action.payload
+        }
       };
     default:
       return state;
